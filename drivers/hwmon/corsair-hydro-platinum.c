c4cfa24ef7d0b5535a819298872abf7168c37770
Jack Greiner <jack@emoss.org>
2026-03-25 06:20:18 -0400

    corsair-hydro-platinum: Fail probe if device initialization fails
    Previously, initialization and initial status update failures were
    logged as warnings but probe continued. This could leave the device
    in an undefined state. Fail probe entirely if either step fails.
    
    Signed-off-by: Jack Greiner <jack@emoss.org>

--- drivers/hwmon/corsair-hydro-platinum.c
+++ drivers/hwmon/corsair-hydro-platinum.c
@@ -719,19 +719,21 @@ static int hydro_platinum_probe(struct hid_device *hdev, const struct hid_device
 		priv->target_fan_duty[i] = 128; /* 50% approx */
 	}
 
-	hid_info(hdev, "Initializing device (Set Cooling: Pump Balanced, Fans 50%%)...\n");
 	ret = hydro_platinum_write_cooling(priv);
-	if (ret)
-		hid_warn(hdev, "initialization command failed: %d\n", ret);
+	if (ret) {
+		hid_err(hdev, "initialization command failed: %d\n", ret);
+		goto fail_and_close;
+	}
 
 	/* Initial update to get firmware version */
-	hid_info(hdev, "Requesting initial status update...\n");
 	ret = hydro_platinum_update(priv);
-	if (ret)
-		hid_warn(hdev, "initial update failed: %d\n", ret);
-	else
-		hid_info(hdev, "Firmware version: %d.%d.%d\n",
-			 priv->fw_version[0], priv->fw_version[1], priv->fw_version[2]);
+	if (ret) {
+		hid_err(hdev, "initial status update failed: %d\n", ret);
+		goto fail_and_close;
+	}
+
+	hid_info(hdev, "Firmware version: %d.%d.%d\n",
+		 priv->fw_version[0], priv->fw_version[1], priv->fw_version[2]);
 
 	priv->hwmon_dev = hwmon_device_register_with_info(&hdev->dev, info->hwmon_name,
 							  priv, &hydro_platinum_chip_info,
